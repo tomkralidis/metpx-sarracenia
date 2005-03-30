7c4b069f766966186250b41a269678c696c9be31
Dan <dlema99@sourceforge.net>
2005-03-30 18:44:20 +0000

    Test functions removed

--- lib/senderAMIS.py
+++ lib/senderAMIS.py
@@ -18,14 +18,11 @@ from MultiKeysStringSorter import MultiKeysStringSorter
 
 class senderAMIS: 
    
-   data = "Salut, bonjour!\r\r\n" # 16 bytes
-   test = 0
-
    def __init__(self, options, logger):
       self.options = options                          # Options
       self.remoteHost = options.host                  # Remote host (name or ip) 
       self.port = int(options.port)                   # Port to which the receiver is bind
-      self.maxLength = 1024                           # Maximum length that we can transmit on the link
+      self.maxLength = 1000000                        # Maximum length that we can transmit on the link
       self.address = (self.remoteHost, self.port)     # Socket address
       self.timeout = options.connect_timeout          # No timeout for now
       self.logger = logger                            # Logger object
@@ -40,13 +37,12 @@ class senderAMIS:
       self.run()
 
    def _connect(self):
-
       self.socketAMIS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.socketAMIS.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
       self.socketAMIS.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-      print self.socketAMIS.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+      #print self.socketAMIS.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
       #self.socketAMIS.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF,4096)
-      print self.socketAMIS.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+      #print self.socketAMIS.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
       #self.socketAMIS.setblocking(True)
 
       while True:
@@ -66,9 +62,6 @@ class senderAMIS:
    def shutdown(self):
       pass
 
-   def readTest(self):
-      return senderAMIS.data
-
    def read(self):
       self.reader = DiskReader(fet.FET_DATA + fet.FET_TX + self.options.client,
                                fet.clients[self.options.client][5],
@@ -77,15 +70,6 @@ class senderAMIS:
                                eval(fet.clients[self.options.client][4]))
       self.reader.sort()
       return(self.reader.getFilesContent(fet.clients[self.options.client][5]))
-
-   def writeTest(self, data):
-      nbBytes = self.socketAMIS.send(data)
-      self.totBytes += nbBytes
-      print self.totBytes
-      if (self.totBytes > 108000):
-         result = open('/apps/px/result', 'w')
-         result.write(self.printSpeed())
-         sys.exit()
 
    def write(self, data):
       if len(data) >= 1:
@@ -98,18 +82,25 @@ class senderAMIS:
                bullAMIS = bullAMIS[nbBytesSent:]
                nbBytesToSend = len(bullAMIS)
                self.totBytes += nbBytesSent
-               print self.totBytes
+               #print self.totBytes
             self.logger.writeLog(self.logger.INFO,"%s has been sent", os.path.basename(self.reader.sortedFiles[index]))
-            #os.unlink(self.reader.sortedFiles[index])
-            self.logger.writeLog(self.logger.DEBUG,"%s has been erased", os.path.basename(self.reader.sortedFiles[index]))
+            try:
+               os.unlink(self.reader.sortedFiles[index])
+               self.logger.writeLog(self.logger.DEBUG,"%s has been erased", os.path.basename(self.reader.sortedFiles[index]))
+            except OSError, e:
+               (type, value, tb) = sys.exc_info()
+               self.logger.writeLog(self.logger.ERROR, "Unable to unlink %s ! Type: %s, Value: %s" 
+                                    % (self.reader.sortedFiles[index], type, value))
 
       else:
          time.sleep(1)
 
+      """
       if (self.totBytes > 54000):
          result = open('/apps/px/result', 'w')
          result.write(self.printSpeed())
          sys.exit()
+      """
 
    def printSpeed(self):
       elapsedTime = time.time() - self.initialTime
@@ -132,27 +123,19 @@ class senderAMIS:
       return preamble + data + endOfMessage
 
    def run(self):
-      if senderAMIS.test:
-         while True:
-            data = self.readTest()
-            try:
-               self.writeTest(data)
-            except socket.error:
-               print "Sending error: %s" % e
-               
-            #time.sleep(0.002)
-            #break
-      else:
-         while True:
-            data = self.read()
-            try:
-               self.write(data)
-            except socket.error:
-               print "Sending error: %s" % e
-               # FIXME: We can try to reconnect
+      while True:
+         data = self.read()
+         try:
+            self.write(data)
+         except socket.error, e:
+            (type, value, tb) = sys.exc_info()
+            self.logger.writeLog(self.logger.ERROR, "Sender error %s ! Type: %s, Value: %s" 
+                                 % (self.reader.sortedFiles[index], type, value))
+            
+            self.logger.writeLog(self.logger.ERROR, "Sending error: %s" % e)
+            # FIXME: We can try to reconnect
 
-            #time.sleep(0.2)
-            #break
+         #time.sleep(0.2)
 
 if __name__ == "__main__":
    sender = senderAMIS("cisco-test.test.cmc.ec.gc.ca", 4001)
