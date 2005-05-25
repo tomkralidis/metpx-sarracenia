3fd5d53a3695b142c52fb5da9ca4aefc41d0c59d
Dan <dlema99@sourceforge.net>
2005-05-25 15:53:33 +0000

    This Sorter use the standard python sort() for list

---
+++ lib/StandardSorter.py
@@ -0,0 +1,50 @@
+"""
+#############################################################################################
+# Name: StandardSorter.py
+#
+# Author: Daniel Lemay
+#
+# Date: 2005-05-05
+#
+# Description:
+#
+#############################################################################################
+
+"""
+import os, commands
+from SortableString import SortableString
+import time
+
+class StandardSorter:
+
+    def __init__(self, list):
+        self.list = list
+
+    def sort(self):
+        self.list.sort()
+        return self.list 
+
+if __name__ == "__main__":
+
+    """
+    (status, output) = commands.getstatusoutput("date")
+    print output
+    files = os.listdir("/apps/pds/tools/ColumboNCCS/testfiles1/")
+    (status, output) = commands.getstatusoutput("date")
+    print output
+    sortedFiles = MultiKeysStringSorter(files).sort()
+    (status, output) = commands.getstatusoutput("date")
+    print output
+    #print sortedFiles
+    # regarder si lstat rallonge beaucoup
+    """
+
+    l = ['pomme', 'poire', 'abricot', 'tomate', 'banane']
+
+    sorter = StandardSorter(l)
+    print sorter.list
+    sorter.sort()
+    print sorter.list
+
+
+    
